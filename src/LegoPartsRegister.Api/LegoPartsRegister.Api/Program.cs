10d1e4db8e316364d5114497cd7f2f1312eb2550
Przemek Urbanek <73723125+xeorast@users.noreply.github.com>
2021-12-22 19:05:14 +0100

    extracted configuration methods

--- src/LegoPartsRegister.Api/LegoPartsRegister.Api/Program.cs
+++ src/LegoPartsRegister.Api/LegoPartsRegister.Api/Program.cs
@@ -1,39 +1,16 @@
 global using LegoPartsRegister.Api.Services;
-
+using LegoPartsRegister.Api;
 using LegoPartsRegister.Data;
 
 var builder = WebApplication.CreateBuilder( args );
 
 // Add services to the container.
-
-_ = builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-_ = builder.Services.AddEndpointsApiExplorer();
-_ = builder.Services.AddSwaggerGen();
-
-
-_ = builder.Services.AddNpgsql<AppDbContext>(
-	builder.Configuration.GetConnectionString( "postgresConnection" ),
-	  pgob => pgob.MigrationsAssembly( "LegoPartsRegister.Migrations.Pg" ),
-	  ob => ob.UseLoggerFactory( LoggerFactory.Create( factoryBuilder => factoryBuilder.AddConsole() ) )
-	);
-
-_ = builder.Services.AddScoped<IUsersService, UsersService>();
+Startup.ConfigureServices( builder );
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if ( app.Environment.IsDevelopment() )
-{
-	_ = app.UseSwagger();
-	_ = app.UseSwaggerUI();
-}
-
-_ = app.UseHttpsRedirection();
-
-_ = app.UseAuthorization();
-
-_ = app.MapControllers();
+Startup.Configure( app );
 
 app.Run();
---
+++ src/LegoPartsRegister.Api/LegoPartsRegister.Api/Startup.cs
@@ -0,0 +1,42 @@
+﻿using LegoPartsRegister.Data;
+
+namespace LegoPartsRegister.Api;
+
+public static class Startup
+{
+	// Add services to the container.
+	public static void ConfigureServices( WebApplicationBuilder builder )
+	{
+		_ = builder.Services.AddControllers();
+		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+		_ = builder.Services.AddEndpointsApiExplorer();
+		_ = builder.Services.AddSwaggerGen();
+
+
+		_ = builder.Services.AddNpgsql<AppDbContext>(
+			builder.Configuration.GetConnectionString( "postgresConnection" ),
+			  pgob => pgob.MigrationsAssembly( "LegoPartsRegister.Migrations.Pg" ),
+			  ob => ob.UseLoggerFactory( LoggerFactory.Create( factoryBuilder => factoryBuilder.AddConsole() ) )
+			);
+
+		_ = builder.Services.AddScoped<IUsersService, UsersService>();
+
+	}
+
+	// Configure the HTTP request pipeline.
+	public static void Configure( WebApplication app )
+	{
+		if ( app.Environment.IsDevelopment() )
+		{
+			_ = app.UseSwagger();
+			_ = app.UseSwaggerUI();
+		}
+
+		_ = app.UseHttpsRedirection();
+
+		_ = app.UseAuthorization();
+
+		_ = app.MapControllers();
+	}
+
+}
